96fb87282409cc669a0987cb6c6aea4fa3194f82
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-12-23 11:04:29 +0530

    completed

--- calorie-tracker-master/src/calorieTracker.js
+++ calorie-tracker-master/src/calorieTracker.js
@@ -1,11 +1,15 @@
-const calculateWeightLostInAMonth = (cycling,running,swimming,extraCalorieInTake) =>{
-   let weightLostInAMonth = 0;
+const calculateWeightLostInAMonth = (cycling, running, swimming, extraCalorieInTake) => {
+	let weightLostInAMonth = 0
 
-   // write logic here 
+	// write logic here
+	if (running == 0 || swimming == 0 || cycling == 0) {
+		return (weightLostInAMonth = -1)
+	}
 
-   return weightLostInAMonth;
-   
+	const totalBurnt = (cycling + running + swimming) * 4 * 2
+	const netBurnt = totalBurnt - extraCalorieInTake * 30
+	weightLostInAMonth = +parseFloat(netBurnt / 1000).toFixed(1)
+	return weightLostInAMonth
 }
 
 module.exports = calculateWeightLostInAMonth
-
