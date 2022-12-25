a24e083808c736a181b897342c04fc715b54daa7
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-12-25 21:51:09 +0530

    new

--- employees-of-an-organization-master/src/callbacks.js
+++ employees-of-an-organization-master/src/callbacks.js
@@ -1,24 +1,22 @@
- 
-   // Define a getEmployee function that iterates over the employees list and displays it
-   // Employees must be displayed after a timeout of 1 second.
-   getEmployee = (employees,callback)=> {
-       setTimeout(() => {        
-           // Write code here to display the name of the employee and return a callback        
-           return callback();           
-       });
-   }
-   
-   // Define a function that creates a new employee after 2 seconds.
-   // and then makes a call to callback function to display the same.
-   createEmployee = (employees,employee, callback) => {   
-       setTimeout(()=>
-       {
-           // write your code here to push new employee to employees list and return a callback.           
-           return callback();
-       })
-   }  
+// Define a getEmployee function that iterates over the employees list and displays it
+// Employees must be displayed after a timeout of 1 second.
+getEmployee = (employees, callback) => {
+	// Write code here to display the name of the employee and return a callback
+	setTimeout(() => {
+		const empName = employees.map((employee) => employee.name)
+		return callback(undefined, empName)
+	}, 1000)
+}
 
-   module.exports = {getEmployee,createEmployee}
-  
+// Define a function that creates a new employee after 2 seconds.
+// and then makes a call to callback function to display the same.
+createEmployee = (employees, employee, callback) => {
+	setTimeout(() => {
+		employees.push(employee)
 
+		// write your code here to push new employee to employees list and return a callback.
+		return callback(undefined, employees)
+	}, 2000)
+}
 
+module.exports = { getEmployee, createEmployee }
