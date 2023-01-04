1b8cb1658cd783b6a528463b373b31a9d608b7a5
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-01-04 16:20:41 +0530

    week 7

---
+++ calculator-app-part1-boilerplate-master/src/calculatorApp.js
@@ -0,0 +1,9 @@
+const addition = (num1, num2) => {
+  // Write the code here
+};
+
+const subtraction = (num1, num2) => {
+    // Write the code here
+};
+
+module.exports = { multiplication, division };
